bd05cf25a614be5369f834ca50048b3148bd3ee0
mpaladino23 <mpaladino23@my.whitworth.edu>
2023-04-27 13:31:04 -0700

    Made a prototype frame for the end application

---
+++ TestVisio/TestVisio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+using Microsoft.Office.Interop.Visio;
+
+namespace TestVisio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
