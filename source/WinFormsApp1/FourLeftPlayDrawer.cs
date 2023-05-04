55899d8cf874e18c3bbb0bb98f7556e95c2181fe
tngoche23 <tngoche23@my.whitworth.edu>
2023-05-04 10:42:20 -0700

    Json file and 4 play drawer

---
+++ source/WinFormsApp1/FourLeftPlayDrawer.cs
@@ -0,0 +1,43 @@
+﻿using Aspose.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp1;
+
+namespace WinFormsWithAspose
+{
+    public class FourLeftPlayDrawer : IPlayDrawer
+    {
+        private readonly Page page;
+        private readonly List<Play> plays;
+
+        public FourLeftPlayDrawer(Page page, List<Play> plays)
+        {
+            this.page = page;
+            this.plays = plays;
+        }
+        public void Draw()
+        {
+            double height = this.page.PageSheet.PageProps.PageHeight.Value;
+            double width = this.page.PageSheet.PageProps.PageWidth.Value;
+            double margin = 0.5;
+            double x = 0, y = 0;
+            double heightOffset = -1;
+            x = width / 3 - margin;
+            for (int i = 0; i < 4; i++)
+            {
+                y = height - (height / 4 * (i + 1) + heightOffset);
+                if (plays.Count > i)
+                {
+                    plays[i].X = x;
+                    plays[i].Y = y;
+                    plays[i].Scale = 0.5;
+                    plays[i].Draw(page);
+                }
+            }
+            //.DrawRouteGroup(play.GetRouteGroup())
+        }
+    }
+}
--- source/WinFormsApp1/Program.cs
+++ source/WinFormsApp1/Program.cs
@@ -1,6 +1,7 @@
 using Aspose.Diagram;
 using System.Security.Cryptography.Pkcs;
 using WinFormsWithAspose;
+using Newtonsoft.Json;
 
 namespace WinFormsApp1
 {
@@ -14,18 +15,28 @@ namespace WinFormsApp1
             Diagram diagram = new Diagram();
             Page firstPage = diagram.Pages.GetPage(0);
 
-            string formation = ""; // pull from excel file
-            string concept = "";
-            // GET
+            // Write to JSON
+            formation firstFormation = new formation("Duo", 3, 0, 2.05, -.35, -.5, -1.25, 0, -1, -1.25, -.35, -3, 0);
+            List<formation> formations = new List<formation>();
+            formations.Add(firstFormation);
+            DataTable<formation> dataTables = new DataTable<formation>("Formations", formations);
+            string formationJson = JsonConvert.SerializeObject(dataTables, Formatting.Indented);
+            File.WriteAllText(@"../../../formation.json", formationJson);
 
+            // Read from JSON
+            string readFromFormation = File.ReadAllText(@"../../../formation.json");
+            DataTable<formation> formationsFromJson = JsonConvert.DeserializeObject<DataTable<formation>>(readFromFormation);
 
-            List <Play> plays = new List<Play>();
-            for(int i = 0; i < 8; i++)
+            List<formation> x = formationsFromJson.table;
+
+            List<Play> plays = new List<Play>();
+            for(int i = 0; i < 4; i++)
             {
-                AddSamplePlay(plays);
+                AddSamplePlay(plays, x[0]);
+                //AddSamplePlay(plays, x[i]);
             }
 
-            EightPlayDrawer pd = new EightPlayDrawer(firstPage, plays);
+            FourLeftPlayDrawer pd = new FourLeftPlayDrawer(firstPage, plays);
             pd.Draw();
 
             diagram.ExportDiagram(dataDir);
@@ -33,19 +44,94 @@ namespace WinFormsApp1
             //ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
         }
-        public static void AddSamplePlay(List<Play> plays)
+        public static void AddSamplePlay(List<Play> plays, formation formation)
         {
-            double x = 4.25;
+            /*
             plays.Add(new Play(new List<IDiagramGroup>{
                 new SkillPlayerGroup(0.4, 0.3)
-                    .Add("X", 7.25 - x, 4.25 - x)
-                    .Add("Y", 3 - x, 3.9 - x)
-                    .Add("Z", 1.25 - x, 4.25 - x)
-                    .Add("Q", 4.25 - x, 3.25 - x)
-                    .Add("T", 3.75 - x, 3 - x)
-                    .Add("H", 6.3 - x, 3.9 - x),
+                    .Add("X", 3, 0)
+                    .Add("Y", -1.25, -.35)
+                    .Add("Z", -3, 0)
+                    .Add("Q", 0, -1)
+                    .Add("T", -0.5, -1.25)
+                    .Add("H", 2.05, -.35),
+                new CenterFormation(0.4, 0.3, 0.5)
+            }));
+            */
+            plays.Add(new Play(new List<IDiagramGroup>
+            {
+                new SkillPlayerGroup(0.4, 0.3)
+                    .Add("X", formation.x_x, formation.x_y)
+                    .Add("Y", formation.y_x, formation.y_y)
+                    .Add("Z", formation.z_x, formation.z_y)
+                    .Add("Q", formation.q_x, formation.q_y)
+                    .Add("T", formation.t_x, formation.t_y)
+                    .Add("H", formation.h_x, formation.h_y),
                 new CenterFormation(0.4, 0.3, 0.5)
             }));
         }
+
+
+        public class formation
+        {
+            public string? name;
+            public double x_x;
+            public double x_y;
+            public double h_x;
+            public double h_y;
+            public double t_x;
+            public double t_y;
+            public double q_x;
+            public double q_y;
+            public double y_x;
+            public double y_y;
+            public double z_x;
+            public double z_y;
+
+            public formation() { }
+
+            public formation(string name, double x_x, double x_y, double h_x, double h_y, double t_x, double t_y, double q_x, double q_y, double y_x, double y_y, double z_x, double z_y)
+            {
+                this.name = name;
+                this.x_x = x_x;
+                this.x_y = x_y;
+                this.h_x = h_x;
+                this.h_y = h_y;
+                this.t_x = t_x;
+                this.t_y = t_y;
+                this.q_x = q_x;
+                this.q_y = q_y;
+                this.y_x = y_x;
+                this.y_y = y_y;
+                this.z_x = z_x;
+                this.z_y = z_y;
+
+            }
+        }
+        [Serializable]
+        class DataTable<Type>
+        {
+            public DataTable() { }
+            public DataTable(string name, List<Type> list)
+            {
+                this.name = name;
+                table = list;
+            }
+
+            public void add(Type toAdd)
+            {
+                table.Add(toAdd);
+            }
+
+            public void delete(int i)
+            {
+                table.RemoveAt(i);
+            }
+
+            public List<Type> table { get; set; }
+            public string name { get; set; }
+        };
+
+
     }
 }
