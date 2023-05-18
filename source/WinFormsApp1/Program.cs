cc0a2f66ff807885d1e5988511b22647daedbdb4
mpaladino23 <mpaladino23@my.whitworth.edu>
2023-05-18 09:26:48 -0700

    Fixed the bugs with the UI

--- source/WinFormsApp1/Program.cs
+++ source/WinFormsApp1/Program.cs
@@ -10,101 +10,147 @@ namespace WinFormsApp1
         [STAThread]
         static void Main()
         {
-
-            string fileName = @"../../../Spring_Football_2023.xlsx";
-
-
             // Read from JSON, store Formation objects into a List
             string readFromFormation = File.ReadAllText(@"../../../formation.json");
             List<Formation> formationsTable = JsonConvert.DeserializeObject<DataTable<Formation>>(readFromFormation).table;
 
+            //Set up the open file dialog so they can choose an excel file to parce from
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
             //Title of the window
             openFileDialog.Title = "Select a playsheet";
 
-            PlaySheet playsheet = new PlaySheet(fileName);
-            playsheet.ParsePlays();
-            playsheet.PrintPlays();
+            //Set up the initial directory the file dialog will start in
+            openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory + @"../../../";
 
-            List<Category> categories = new();
-            Category category;
-            playsheet.Plays.ForEach(x =>
+            //Restrict the file type to excel formats
+            openFileDialog.Filter = "excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+
+            //Keep running until prompted to close
+            bool running = true;
+            while (running)
             {
-                category = new(x.categoryName);
-                x.formations.ForEach(y =>
+                //Show the file dialog
+                System.Windows.Forms.DialogResult result = openFileDialog.ShowDialog();
+                //If they select the file, then process it, otherwise, confirm they want to exit the applicaion
+                if (result == System.Windows.Forms.DialogResult.OK)
                 {
-                    string substr;
-                    if (y.Contains(" LT"))
-                    {
-                        substr = y.Substring(0, y.IndexOf(" LT"));
-                    }
-                    else if (y.Contains(" RT"))
-                    {
-                        substr = y.Substring(0, y.IndexOf(" RT"));
-                    }
-                    else
-                    {
-                        Console.WriteLine("Something failed.");
-                        return;
-                    }
+                    String fileName = openFileDialog.FileName;
 
-                    int len = substr.Split(" ").Length;
-                    for (int i = 0; i < len; i++)
+                    PlaySheet playsheet = new PlaySheet(fileName);
+                    playsheet.ParsePlays();
+                    playsheet.PrintPlays();
+
+                    List<Category> categories = new();
+                    Category category;
+                    playsheet.Plays.ForEach(x =>
                     {
-                        // see if substring exists in formations table
-                        Formation form = formationsTable.Find(f => (f.name.ToUpper() == substr.ToUpper()));
-                        if (form != null)
+                        category = new(x.categoryName);
+                        x.formations.ForEach(y =>
                         {
-                            category.Add(form);
-                            break;
-                        }
-                        //
-                        substr = substr.Substring(substr.IndexOf(" ")+1);
+                            string substr;
+                            if (y.Contains(" LT"))
+                            {
+                                substr = y.Substring(0, y.IndexOf(" LT"));
+                            }
+                            else if (y.Contains(" RT"))
+                            {
+                                substr = y.Substring(0, y.IndexOf(" RT"));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Parcer failed to find LF or RT keywork!");
+                                return;
+                            }
+
+                            int len = substr.Split(" ").Length;
+                            for (int i = 0; i < len; i++)
+                            {
+                                // see if substring exists in formations table
+                                Formation form = formationsTable.Find(f => (f.name.ToUpper() == substr.ToUpper()));
+                                if (form != null)
+                                {
+                                    category.Add(form);
+                                    break;
+                                }
+                                //
+                                substr = substr.Substring(substr.IndexOf(" ")+1);
+                            }
+                        });
+                        categories.Add(category);
+                    });
+
+                    // TODO: 1. Need parser code here to add plays to different categories.
+                    //       2. I recommend storing them as a list of categories (List<Category>)
+                    // Currently, this code adds every formation in the table to just one Category object
+                    // * * * * * *
+                    //Category category = new();
+                    foreach(Formation formation in formationsTable)
+                    {
+                        //category.Add(formation); 
                     }
-                });
-                categories.Add(category);
-            });
-
-            // TODO: 1. Need parser code here to add plays to different categories.
-            //       2. I recommend storing them as a list of categories (List<Category>)
-            // Currently, this code adds every formation in the table to just one Category object
-            // * * * * * *
-            //Category category = new();
-            foreach(Formation formation in formationsTable)
-            {
-                //category.Add(formation); 
-            }
-            // * * * * * *
+                    // * * * * * *
 
 
-            Diagram diagram = new();
-            FourLeftPlayDrawer pd;
-            Page currentPage;
-            // TODO: For each category from the list you created, run the following block of code.
-            // * * * * * *
-            foreach(Category c in categories)
-            {
-                double width, height;
-                for (int i = 0; i < c.Count; i++)
+                    Diagram diagram = new();
+                    FourLeftPlayDrawer pd;
+                    Page currentPage;
+                    // TODO: For each category from the list you created, run the following block of code.
+                    // * * * * * *
+                    foreach(Category c in categories)
+                    {
+                        double width, height;
+                        for (int i = 0; i < c.Count; i++)
+                        {
+                            currentPage = diagram.Pages[diagram.Pages.Add(new()) - 1];
+                            width = currentPage.PageSheet.PageProps.PageWidth.Value;
+                            height = currentPage.PageSheet.PageProps.PageHeight.Value;
+                            currentPage.AddText(width/2, height/100*95, 1, 1, c.Name);
+                            pd = new FourLeftPlayDrawer(currentPage, c.GetPlayPage(i));
+                            pd.Draw();
+                        }
+                    }
+            
+                    // * * * * * *
+
+
+                    // removes empty page that gets generated at the end
+                    diagram.Pages.Remove(diagram.Pages[diagram.Pages.Count-1]);
+                    diagram.ExportDiagram("..\\");
+
+                    // ignore for now
+                    //ApplicationConfiguration.Initialize();
+                    //Application.Run(new Form1());
+
+
+
+                    //Set up message box to respond to the user
+                    string message = "Plays generated successfully! Would you like to open another file?";
+                    string title = "Whitworth Football Playmaker";
+                    DialogResult quit = MessageBox.Show(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+
+                    //If they want to quit then set running to false
+                    if (quit == DialogResult.No)
+                    {
+                        running = false;
+                    }
+                }
+                else
                 {
-                    currentPage = diagram.Pages[diagram.Pages.Add(new()) - 1];
-                    width = currentPage.PageSheet.PageProps.PageWidth.Value;
-                    height = currentPage.PageSheet.PageProps.PageHeight.Value;
-                    currentPage.AddText(width/2, height/100*95, 1, 1, c.Name);
-                    pd = new FourLeftPlayDrawer(currentPage, c.GetPlayPage(i));
-                    pd.Draw();
+                    //Set up and show message box
+                    string message = "Exit the application?";
+                    string title = "Whitworth Football Playmaker";
+                    DialogResult quit = MessageBox.Show(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+
+                    //If they want to close then set running to false
+                    if (quit == DialogResult.Yes)
+                    {
+                        running = false;
+                    }
                 }
             }
+
             
-            // * * * * * *
-
-
-            // removes empty page that gets generated at the end
-            diagram.Pages.Remove(diagram.Pages[diagram.Pages.Count-1]);
-            diagram.ExportDiagram("..\\");
-
-            // ignore for now
-            //ApplicationConfiguration.Initialize();
-            //Application.Run(new Form1());
         }
     }
 }
